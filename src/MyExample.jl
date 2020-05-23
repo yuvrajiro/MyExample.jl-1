40b072f58c1b56864d414483e84a1ae7cf610789
Jesse Chan <jesse.chan@rice.edu>
2020-05-23 10:49:40 -0500

    adding my_f for demo

--- src/MyExample.jl
+++ src/MyExample.jl
@@ -1,5 +1,7 @@
 module MyExample
+export my_f
 
 greet() = print("Hello World!")
+include("extra_file.jl")
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,7 @@ using Test
 
 @testset "MyExample.jl" begin
     # Write your own tests here.
+    @test my_f(2,1) == 7
+    @test my_f(2,3) == 13
+    
 end
